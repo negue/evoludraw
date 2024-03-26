db291a678ac5e52fce87a0db73580ad946ff7fa1
negue <eugen.bolz@gmail.com>
2024-03-26 23:00:11 +0100

    correct base href - Take2

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export const __dirname = new URL('.', import.meta.url).pathname;
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/evoludraw/',
   optimizeDeps: {
     // A workaround for Vite bug: https://github.com/vitejs/vite/issues/13314#issuecomment-1560745780
     exclude: ["@evolu/react"],
